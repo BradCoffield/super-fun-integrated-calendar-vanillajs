9d212a465b7ba8ce635e5345bdfca35408982cb4
Brad Coffield <bcoffield@gmail.com>
2019-07-06 13:48:03 -0600

    button source toggle working with jquery

--- calendar.js
+++ calendar.js
@@ -1,37 +1,24 @@
 console.log("hi");
 
 document.addEventListener("DOMContentLoaded", function() {
-
   // Tingle modal - generic generator code
   var modal = new tingle.modal({
-    footer: true,
-    // stickyFooter: true,
-    // closeMethods: ['overlay', 'button', 'escape'],
-    // closeLabel: "Close",
-    // cssClass: ['custom-class-1', 'custom-class-2'],
-    // onOpen: function() {
-    //     console.log('modal open');
-    // },
-    // onClose: function() {
-    //     console.log('modal closed');
-    // },
-    // beforeClose: function() {
-    //     // here's goes some logic
-    //     // e.g. save content before closing the modal
-    //     return true; // close the modal
-    //     return false; // nothing happens
-    // }
-});
-// Creating a close button for any modal we create. Button text, css classes, logic on-click.
-modal.addFooterBtn('Close', 'tingle-btn tingle-btn--primary tingle-btn--pull-right', function() {
-  modal.close();
-});
+    footer: true
+  });
 
+  // Creating a close button for any modal we create. Button text, css classes, logic on-click.
+  modal.addFooterBtn(
+    `<strong>Close</strong>`,
+    "tingle-btn tingle-btn--primary tingle-btn--pull-right",
+    function() {
+      modal.close();
+    }
+  );
 
   var calendarEl = document.getElementById("calendar");
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    plugins: ["dayGrid", "googleCalendar"],
+    plugins: ["dayGrid", "googleCalendar", "list"],
     googleCalendarApiKey: "AIzaSyAm6_e7XOpN1lB0cSUA3Ij8vsPdHAfznoU",
     /* this is where we put all the source calendars information, as a series of objects */
     eventSources: [
@@ -42,48 +29,62 @@ modal.addFooterBtn('Close', 'tingle-btn tingle-btn--primary tingle-btn--pull-rig
     ],
     defaultView: "dayGridMonth",
     header: {
-      left: "prev,next,today",
-      center: "title",
-      right: "dayGridMonth,dayGridWeek,dayGridDay"
+      left: "title,prev,next,today",
+      center: "",
+      right: "dayGridMonth,dayGridWeek,dayGridDay,listMonth"
     },
     /* What happens when someone clicks a particular event. In this case, open a modal with additional information about the event. */
     eventClick: function(info) {
-        console.log(info.event)
+      console.log(info.event);
       let theLocation, theDescription, theTitle;
-      if (info.event.extendedProps.location) {theLocation = info.event.extendedProps.location} else {theLocation = ""}
-      if (info.event.title) {theTitle = info.event.title} else {theTitle = ""}
-     if (info.event.extendedProps.description) {theDescription = info.event.extendedProps.description} else {theDescription = ""}
-     let theURL = info.event.url;
-      modal.setContent(`<h3>${info.event.title}</h3><p>${theLocation}</p><p>${theDescription}</p><p><a href="${theURL}" target="_blank">More Information</a></p>`);    
-      
+      if (info.event.extendedProps.location) {
+        theLocation = info.event.extendedProps.location;
+      } else {
+        theLocation = "";
+      }
+      if (info.event.title) {
+        theTitle = info.event.title;
+      } else {
+        theTitle = "";
+      }
+      if (info.event.extendedProps.description) {
+        theDescription = info.event.extendedProps.description;
+      } else {
+        theDescription = "";
+      }
+      let theURL = info.event.url;
+      modal.setContent(
+        `<h3>${
+          info.event.title
+        }</h3><p>${theLocation}</p><p>${theDescription}</p><p><a href="${theURL}" target="_blank">More Information</a></p>`
+      );
+
       modal.open();
-      // alert(info.event.title);
+
       info.jsEvent.preventDefault();
     }
-   
-   //SWEETALERT things
-    // eventClick: function(info) {
-    //   console.log(info.event.extendedProps.description)
-    //   let theLocation, theDescription, theTitle;
-    //   if (info.event.extendedProps.location) {theLocation = info.event.extendedProps.location} else {theLocation = ""}
-    //   if (info.event.title) {theTitle = info.event.title} else {theTitle = ""}
-    //  if (info.event.extendedProps.description) {theDescription = info.event.extendedProps.description} else {theDescription = ""}
-
-    //   /*Open Sweet Alert*/
-    //       swal({
-    //         title: theTitle,//Event Title
-    //           text: `<strong>${theLocation}</strong>`,
-              
-            
-    //         // text: "Start From : "+moment(calEvent.start).format("MMMM Do YYYY, h:mm a"),//Event Start Date
-    //         icon: "success",
-    //       });
-    //       info.jsEvent.preventDefault();
-    //   }
- 
   });
 
   calendar.render();
 });
-//   ok, so dayGridDay and dayGridWeek as default views show the by week and by day that I want. Just need to make them accessible from buttons.
 
+let theCalendar = document.getElementById("calendar");
+
+// Hooking up with the buttons for toggling the display of different calendars
+// let g2Toggle = document.getElementById("g2-toggle");
+
+// // Okay, so this successfully removes/adds that class from the calendar but...nothing visibly changes
+// g2Toggle.addEventListener("click", event => {
+//   // console.log(theCalendar.classList)
+//   // theCalendar.classList.toggle("rmc-student-events");
+  
+// });
+
+
+//Well, this works with jquery...
+$("#g2-toggle").click(function() {
+  $(".rmc-student-events").toggle();
+  console.log('hiiii')
+  // $("#rmc-student-events").toggleClass('btn-primary btn-default');
+});
+ 
